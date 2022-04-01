16991b3714e7ef8722d4123d5271ff5c1e4f49eb
Ilona Brand <theilonabrand@gmail.com>
2022-03-31 20:07:31 -0500

    v1 of a decent smooth pan that only does left and right pans with no center panning. The v2 should have thresholds so that you can keep tilting the phone and things keep changing

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 addEventListener('deviceorientation', event => {
-  // left and right tilt
-  // const extraImgWidth = .8; // representing the extra 80% of the 180% that the img is taking up
-  let browserWidth = $(window).width();
-  // let extraSpace = browserWidth * extraImgWidth;
-  // let roomOnEachSide = extraSpace/2;
-  // let imageX = browserWidth * .6
-
   let current = parseInt($('body').css('background-position-x').slice(0, -2));
-  const limit = 20;
-  const distance = 50;
+  const limit = 10;
+  const distance = 60;
   let right = false;
   let left = false;
-  let center = false;
   let position = Math.round(event.gamma);
   if (position > limit) {
     right = true;
-  }
-  else if (position < limit && position > 0) {
-    center = true;
   }
   else if (position < limit) {
     left = true;
@@ -29,18 +18,4 @@ addEventListener('deviceorientation', event => {
   if (left) {
     $('body').css('background-position-x', -200 - distance + 'px')
   }
-  if (center) {
-    $('body').css('background-position-x', -200 + 'px')
-  }
-  // $('body').css('background-position-x', current + position + 'px')
-  $('h2').text(position)
 });
-
-function ofMap(value, min1, max1, min2, max2) {
-  return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
-}
-// window.addEventListener("deviceorientation", handleOrientation, true);
-
-// function handleOrientation(event) {
-
-// }
