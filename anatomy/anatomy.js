e51970eead4dbbf32c5c10d4f5e41b9ec817e937
Ilona Brand <theilonabrand@gmail.com>
2022-04-10 20:23:25 -0500

    add the concept of proximity

--- anatomy/anatomy.js
+++ anatomy/anatomy.js
@@ -1,13 +1,29 @@
 const connections = document.getElementsByClassName("connection");
 
-const executeConnection = (e) => {
+function executeConnection(e) {
     const poemToLinkToId = e.target.getAttribute("data-link-id");
     const targetElement = document.querySelector(`[data-id="${poemToLinkToId}"]`);
+    adjustProximity(targetElement.dataset.proximity);
     // alternate way to do this with visibility which leaves the gaps as opposed to fadeToggle
     // which uses display:none so doesn't leave gaps:
     // targetElement.style.visibility == 'hidden' ? targetElement.style.visibility = 'visible' : targetElement.style.visibility = 'hidden';
     $(targetElement).fadeToggle('2s')
 };
+
+function adjustProximity(proximity) {
+    switch(proximity) {
+        case 'far':
+            $('body').css('background-size', '100%');
+          break;
+        case 'close':
+            $('body').css('background-size', '120%');
+          break;
+        case 'intimate':
+            $('body').css('background-size', '150%');
+        default:
+            $('body').css('background-size', '100%');
+      }
+}
 
 Array.from(connections).forEach((element) => {
     element.addEventListener('click', executeConnection);
